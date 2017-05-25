b87fe87ea4b66147931e0d85471b7d7caf2b7839
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-25 10:15:07 -0400

    jQuery, and test that it works

---
+++ app/assets/javascripts/incidents/edit.coffee
@@ -0,0 +1,35 @@
+$.fn.showIfChecked = (checkbox, selector, options) ->
+  $(this).on 'change', checkbox, () ->
+    checked = $(this).is ':checked'
+    if typeof(options) != 'undefined' && options.reverse
+      checked = !checked
+    if checked
+      $(selector).show()
+    else $(selector).hide()
+
+determineShouldProvideReasonNotUpToCurb = () ->
+  motion = $('#incident_motion_of_bus').val()
+  upToCurb = $('#incident_bus_up_to_curb').is ':checked'
+  formSection = $('.reason-not-up-to-curb-info')
+  if motion == 'Stopped' && !upToCurb
+    formSection.show()
+  else formSection.hide()
+
+$(document).ready ->
+  $('form').showIfChecked '#incident_motor_vehicle_collision',
+                          '.motor-vehicle-collision-info'
+
+  $('form').showIfChecked '#incident_police_on_scene',
+                          '.police-info'
+
+  $('form').showIfChecked '#incident_other_vehicle_owned_by_other_driver',
+                          '.other-vehicle-owner-info', reverse: true
+
+  $('form').showIfChecked '#incident_passenger_incident',
+                          '.passenger-incident-info'
+
+  $('form').on 'change', '#incident_motion_of_bus',
+               determineShouldProvideReasonNotUpToCurb
+
+  $('form').on 'click', '#incident_bus_up_to_curb',
+               determineShouldProvideReasonNotUpToCurb
--- test/system/completing_incidents_test.rb
+++ test/system/completing_incidents_test.rb
@@ -1,7 +1,7 @@
 require 'application_system_test_case'
 
 class CompletingIncidentsTest < ApplicationSystemTestCase
-  test 'drivers can fill out basic fields and submit incidents' do
+  test 'drivers can fill in basic fields and submit incidents' do
     incident = create :incident, :incomplete
 
     when_current_user_is incident.driver
@@ -14,17 +14,107 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
     assert_selector '.info p.notice', text: 'Incident was successfully updated.'
   end
 
-  test 'drivers can fill out motor vehicle collision fields' do
+  test 'drivers can fill in motor vehicle collision fields' do
     incident = create :incident, :incomplete
 
     when_current_user_is incident.driver
     visit edit_incident_url(incident)
 
-    fill_in_basic_fields
-
+    assert_no_text 'Motor Vehicle Collision Information'
     check 'Motor vehicle collision'
     assert_text 'Motor Vehicle Collision Information'
 
+    fill_in_basic_fields
+    fill_in_motor_vehicle_collision_fields
+
+    click_on 'Save Incident'
+
+    assert_selector '.info p.notice', text: 'Incident was successfully updated.'
+  end
+
+  test 'drivers can fill in responding officer information' do
+    incident = create :incident, :incomplete
+
+    when_current_user_is incident.driver
+    visit edit_incident_url(incident)
+
+    check 'Motor vehicle collision'
+
+    assert_no_selector '.police-info'
+    check 'Police on scene'
+    assert_selector '.police-info'
+
+    fill_in_basic_fields
+    fill_in_motor_vehicle_collision_fields
+    fill_in_police_fields
+
+    click_on 'Save Incident'
+
+    assert_selector '.info p.notice', text: 'Incident was successfully updated.'
+  end
+
+  test 'drivers can fill in vehicle owner information' do
+    incident = create :incident, :incomplete
+
+    when_current_user_is incident.driver
+    visit edit_incident_url(incident)
+
+    check 'Motor vehicle collision'
+
+    fill_in_basic_fields
+    fill_in_motor_vehicle_collision_fields
+
+    assert_no_selector '.other-vehicle-owner-info'
+    uncheck 'Other vehicle owned by other driver'
+    assert_selector '.other-vehicle-owner-info'
+
+    fill_in_other_vehicle_owner_fields
+
+    click_on 'Save Incident'
+
+    assert_selector '.info p.notice', text: 'Incident was successfully updated.'
+  end
+
+  test 'drivers can fill in passenger incident information' do
+    incident = create :incident, :incomplete
+
+    when_current_user_is incident.driver
+    visit edit_incident_url(incident)
+
+
+    assert_no_text 'Passenger Incident Information'
+    check 'Passenger incident'
+    assert_text 'Passenger Incident Information'
+
+    fill_in_basic_fields
+    fill_in_passenger_incident_fields
+
+    click_on 'Save Incident'
+
+    assert_selector '.info p.notice', text: 'Incident was successfully updated.'
+  end
+
+  test 'drivers can fill in a reason for not being up to the curb if applicable' do
+    incident = create :incident, :incomplete
+
+    when_current_user_is incident.driver
+    visit edit_incident_url(incident)
+
+    check 'Passenger incident'
+
+    fill_in_basic_fields
+    fill_in_passenger_incident_fields
+
+    assert_no_selector '.reason-not-up-to-curb-info'
+    select 'Stopped', from: 'Motion of bus'
+    uncheck 'Bus up to curb'
+    assert_selector '.reason-not-up-to-curb-info'
+
+    fill_in 'Reason not up to curb', with: "I didn't feel like it."
+
+    click_on 'Save Incident'
+
+    assert_selector '.info p.notice', text: 'Incident was successfully updated.'
   end
 
   def fill_in_basic_fields
@@ -42,5 +132,52 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
     select 'Daylight', from: 'Light conditions'
     check 'Headlights used'
     fill_in 'Description', with: 'I was preparing to make the right turn when a car went around me.'
+  end
+
+  def fill_in_motor_vehicle_collision_fields
+    fill_in 'Other vehicle plate', with: '8CHZ50'
+    fill_in 'Other vehicle state', with: 'MA'
+    fill_in 'Other vehicle make', with: 'Nissan'
+    fill_in 'Other vehicle model', with: 'Integra'
+    fill_in 'Other vehicle year', with: '1993'
+    fill_in 'Other vehicle color', with: 'White'
+    fill_in 'Other vehicle passengers', with: 1
+    select 'East', from: 'Direction'
+    select 'East', from: 'Other vehicle direction'
+    fill_in 'Other driver name', with: 'Nico Rosberg'
+    fill_in 'Other driver license number', with: 'S10354298'
+    fill_in 'Other driver license state', with: 'MA'
+    fill_in 'Other vehicle driver address', with: '23 Wins St'
+    fill_in 'Other vehicle driver address town', with: 'Championsville'
+    fill_in 'Other vehicle driver address state', with: 'MA'
+    fill_in 'Other vehicle driver address zip', with: '00001'
+    fill_in 'Other vehicle driver home phone', with: '413 555 0056'
+    fill_in 'Damage to bus point of impact', with: 'Scratches'
+    fill_in 'Damage to other vehicle point of impact', with: 'Scratches'
+    fill_in 'Insurance carrier', with: 'Progressive'
+    fill_in 'Insurance policy number', with: '58925948'
+    check 'Other vehicle owned by other driver'
+  end
+
+  def fill_in_police_fields
+    fill_in 'Police badge number', with: '1024'
+    fill_in 'Police town or state', with: 'Amherst'
+    fill_in 'Police case assigned', with: 'C34059'
+  end
+
+  def fill_in_other_vehicle_owner_fields
+    fill_in 'Other vehicle owner name', with: 'Lewis Hamilton'
+    fill_in 'Other vehicle owner address', with: '55 Wins St'
+    fill_in 'Other vehicle owner address town', with: 'Championsville'
+    fill_in 'Other vehicle owner address state', with: 'MA'
+    fill_in 'Other vehicle owner address zip', with: '00001'
+    fill_in 'Other vehicle owner home phone', with: '413 555 0056'
+  end
+
+  def fill_in_passenger_incident_fields
+    check 'Occurred front door'
+    check 'Occurred sudden stop'
+    select 'Braking', from: 'Motion of bus'
+    select 'Dry', from: 'Condition of steps'
   end
 end
