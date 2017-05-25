76bef6e73337affbca14f91eb8f728314b7bc947
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-25 10:28:24 -0400

    Test that the form doesn't save if fields are incomplete

--- test/system/completing_incidents_test.rb
+++ test/system/completing_incidents_test.rb
@@ -7,6 +7,16 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
     when_current_user_is incident.driver
     visit edit_incident_url(incident)
 
+    click_on 'Save Incident'
+
+    assert_selector '#error_explanation'
+    within '#error_explanation' do
+      assert_text "Run can't be blank"
+      assert_text "Block can't be blank"
+      assert_text "Bus can't be blank"
+      # etc.
+    end
+
     fill_in_basic_fields
 
     click_on 'Save Incident'
@@ -25,6 +35,16 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
     assert_text 'Motor Vehicle Collision Information'
 
     fill_in_basic_fields
+
+    click_on 'Save Incident'
+
+    assert_selector '#error_explanation'
+    within '#error_explanation' do
+      assert_text "Other vehicle plate can't be blank"
+      assert_text "Other vehicle state can't be blank"
+      assert_text "Other vehicle make can't be blank"
+      # etc.
+    end
     fill_in_motor_vehicle_collision_fields
 
     click_on 'Save Incident'
@@ -46,6 +66,16 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
 
     fill_in_basic_fields
     fill_in_motor_vehicle_collision_fields
+
+    click_on 'Save Incident'
+
+    assert_selector '#error_explanation'
+    within '#error_explanation' do
+      assert_text "Police badge number can't be blank"
+      assert_text "Police town or state can't be blank"
+      assert_text "Police case assigned can't be blank"
+    end
+
     fill_in_police_fields
 
     click_on 'Save Incident'
@@ -68,6 +98,16 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
     uncheck 'Other vehicle owned by other driver'
     assert_selector '.other-vehicle-owner-info'
 
+    click_on 'Save Incident'
+
+    assert_selector '#error_explanation'
+    within '#error_explanation' do
+      assert_text "Other vehicle owner name can't be blank"
+      assert_text "Other vehicle owner address can't be blank"
+      assert_text "Other vehicle owner address town can't be blank"
+      # etc.
+    end
+
     fill_in_other_vehicle_owner_fields
 
     click_on 'Save Incident'
@@ -87,6 +127,15 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
     assert_text 'Passenger Incident Information'
 
     fill_in_basic_fields
+
+    click_on 'Save Incident'
+
+    assert_selector '#error_explanation'
+    within '#error_explanation' do
+      assert_text "Motion of bus can't be blank"
+      assert_text "Condition of steps can't be blank"
+    end
+
     fill_in_passenger_incident_fields
 
     click_on 'Save Incident'
@@ -109,6 +158,13 @@ class CompletingIncidentsTest < ApplicationSystemTestCase
     select 'Stopped', from: 'Motion of bus'
     uncheck 'Bus up to curb'
     assert_selector '.reason-not-up-to-curb-info'
+
+    click_on 'Save Incident'
+
+    assert_selector '#error_explanation'
+    within '#error_explanation' do
+      assert_text "Reason not up to curb can't be blank"
+    end
 
     fill_in 'Reason not up to curb', with: "I didn't feel like it."
 
