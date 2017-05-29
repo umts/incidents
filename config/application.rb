3bce5b6f9ffeb2bfe0a491ce91bbaa2228e0f430
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-29 10:16:40 -0400

    Don't instance eval

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Incidents
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Eastern Time (US & Canada)'
+    config.autoload_paths << Rails.root.join('lib', 'extensions')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/initializers/prawn_rails.rb
+++ config/initializers/prawn_rails.rb
@@ -1,5 +1,7 @@
 require 'prawn-rails'
 
+include PrawnFormBoxes
+
 PrawnRails.config do |config|
   config.page_layout = :portrait
   config.page_size = 'LETTER'
