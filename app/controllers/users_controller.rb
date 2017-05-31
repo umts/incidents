bb2364be3026074cbdcd8eb780b4d371cff373d8
David Faulkenberry <david.faulkenberry@gmail.com>
2017-05-31 14:54:24 -0400

    Order users by name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.includes :incidents
+    @users = User.includes(:incidents).order :name
   end
 
   def update
