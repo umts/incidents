d0af7d2e2c714e5bec5997ac6b9d37c7326f3244
David Faulkenberry <david.faulkenberry@gmail.com>
2017-06-08 10:54:50 -0400

    Edit incident tests pass

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def a11y_datetime_labels(tag_id)
+    %w[Year Month Day Hour Minute].map.with_index do |name, i|
+      content_tag :label, nil, class: 'a11y-read', for: tag_id + "_#{i + 1}i" do
+        name
+      end
+    end
+  end
+
   def yes_no(value)
     value ? 'Yes' : 'No'
   end
