ec751c1279328767f0b9fa7431e7a7df43e93ed0
David Faulkenberry <david.faulkenberry@gmail.com>
2017-06-16 12:35:06 -0400

    Index loads
    Choose relatively coprime indices for 'randomizing' seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,14 +20,16 @@ dates = (4.months.ago.to_date..Date.yesterday).to_a
 dates.shuffle.each.with_index do |day, i|
   driver = incident_drivers.sample
   # Every 20th incident shall be incomplete.
-  incident_type = [nil, :incomplete, :collision, :passenger_incident].sample
+  incident_type = [nil, :collision, :passenger_incident].sample
+  incident_type = :incomplete if (i % 9).zero?
   Timecop.freeze day + rand(24 * 60).minutes do
     PaperTrail.whodunnit = driver.id
     report = if incident_type.present?
       create :incident_report, incident_type, user: driver
     else create :incident_report, user: driver
     end
-    incident = create :incident, driver_incident_report: report
+    incident = create :incident, driver_incident_report: report,
+                                 completed: incident_type != :incomplete
     # TODO: create supervisor incident reports and supervisor reports
     # Every 8th-ish incident shall be unreviewed.
     unless incident_type == :incomplete || (i % 8).zero?
