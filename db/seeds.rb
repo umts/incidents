c7d27c364d190098ef5c6e12c0f70ddab817254f
David Faulkenberry <david.faulkenberry@gmail.com>
2017-06-17 11:25:24 -0400

    Don't create a new pair of supervisors for every incident
    Factory was making one for the supervisor incident report, and another for the supervisor report. Now we just use the existing ten generated by the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,23 +12,30 @@ puts 'Login as staff with:'
 puts " Email:    #{staff.email}"
 puts ' Password: password'
 
-50.times { create :user, :driver, :fake_name }
+supervisors = 10.times.map { |_| create :user, :supervisor, :fake_name }
 
-incident_drivers = User.drivers
+drivers = 50.times.map { |_| create :user, :driver, :fake_name }
 
 dates = (4.months.ago.to_date..Date.yesterday).to_a
 dates.shuffle.each.with_index do |day, i|
-  driver = incident_drivers.sample
+  driver = drivers.sample
+  supervisor = supervisors.sample
   # Every 20th incident shall be incomplete.
   incident_type = [nil, :collision, :passenger_incident].sample
   incident_type = :incomplete if (i % 9).zero?
   Timecop.freeze day + rand(24 * 60).minutes do
     PaperTrail.whodunnit = driver.id
-    report = if incident_type.present?
+    driver_report = if incident_type.present?
       create :incident_report, incident_type, user: driver
     else create :incident_report, user: driver
     end
-    incident = create :incident, driver_incident_report: report,
+    supervisor_report_attrs = driver_report.attributes.symbolize_keys
+                                           .except(:id, :user_id)
+                                           .merge(user: supervisor)
+    supervisor_report = create :incident_report, supervisor_report_attrs
+    incident = create :incident, driver_incident_report: driver_report,
+                                 supervisor_incident_report: supervisor_report,
+                                 supervisor_report: create(:supervisor_report, user: supervisor),
                                  completed: incident_type != :incomplete
     # TODO: create supervisor incident reports and supervisor reports
     # Every 8th-ish incident shall be unreviewed.
