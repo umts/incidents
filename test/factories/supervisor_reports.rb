62af5bf7ac4ec8ed4b8c986fcaa6e4616a3c0492
David Faulkenberry <david.faulkenberry@gmail.com>
2017-06-19 14:37:44 -0400

    Seed more interesting supervisor reports

--- test/factories/supervisor_reports.rb
+++ test/factories/supervisor_reports.rb
@@ -8,6 +8,8 @@ FactoryGirl.define do
     passenger_statement { FFaker::BaconIpsum.paragraphs(5).join "\n" }
     faxed { [nil, Time.zone.now].sample }
     completed_drug_or_alcohol_test { [true, true, false].sample }
+    completed_drug_test { [true, false].sample }
+    completed_alcohol_test { [true, false].sample }
     reason_test_completed { SupervisorReport::REASONS_FOR_TEST.sample }
     testing_facility { SupervisorReport::TESTING_FACILITIES.sample }
     testing_facility_notified_at { Time.zone.now - 6.minutes }
@@ -26,10 +28,28 @@ FactoryGirl.define do
       end
 
       if report.pictures_saved?
-        report.saved_pictures = rand(50).sample
+        report.saved_pictures = rand 50
       end
       
-      report.amplifying_comments = Faker::BaconIpsum.paragraph if rand(20).zero?
+      if rand(20).zero?
+        report.amplifying_comments = FFaker::BaconIpsum.paragraph
+      end
+
+      case report.reason_test_completed
+      when 'Post-Accident'
+        field = %w[
+          test_not_conducted
+          test_due_to_bodily_injury
+          test_due_to_disabling_damage
+          test_due_to_fatality
+        ].sample
+        report.send field + '=', true
+      when 'Reasonable Suspicion'
+        report.observation_made_at = Time.zone.now - 5.minutes
+        reason = %w[appearance behavior speech odor].sample
+        report.send "test_due_to_employee_#{reason}=", true
+        report.send "employee_#{reason}=", FFaker::BaconIpsum.sentence
+      end
     end
   end
 end
