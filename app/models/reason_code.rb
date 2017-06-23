160f8399b0c820a2e5017125b9ea68ecbedce2d6
David Faulkenberry <david.faulkenberry@gmail.com>
2017-06-23 13:09:57 -0400

    Supervisors can submit reports, and staff can complete and add reason code

--- app/models/reason_code.rb
+++ app/models/reason_code.rb
@@ -1,4 +1,8 @@
 class ReasonCode < ApplicationRecord
   has_many :incidents
   validates :identifier, :description, presence: true, uniqueness: true
+
+  def full_label
+    [identifier, description].join ': '
+  end
 end
