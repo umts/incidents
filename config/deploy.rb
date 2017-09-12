bd50d9a4e2d45651a7e574766d094ec3e5a36915
David Faulkenberry <david.faulkenberry@gmail.com>
2017-09-12 11:56:26 -0400

    Set passenger env variable, remove secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,15 +2,13 @@ lock '3.9.0'
 
 set :application, 'incidents'
 set :repo_url, 'git@github.com:umts/incidents.git'
-set :scm, :git
 set :branch, :master
 set :keep_releases, 5
 set :deploy_to, "/srv/#{fetch :application}"
-set :log_level
 
 set :linked_files, fetch(:linked_files, []).push(
   'config/database.yml',
-  'config/application.yml'
+  'config/secrets.yml'
 )
 
 set :linked_dirs, fetch(:linked_dirs, []).push(
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 remote_user = Net::SSH::Config.for('50.198.83.200')[:user] || ENV['USER']
-server '50.198.83.200', user: remote_user
+server '50.198.83.200', user: remote_user, roles: %w(app db web), port: 2250
 set :tmp_dir, "/tmp/#{remote_user}"
+set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/home/umass/passenger_temp' }
