d52e14c6d2d402c0cbaf3d2e00f06c9ac8c1c805
David Faulkenberry <david.faulkenberry@gmail.com>
2017-12-14 16:20:15 -0500

    exception notifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use ExceptionNotification::Rack, email: {
+    email_prefix: 'incidents.pvta.com exception:',
+    sender_address: %{"Incidents Server" <incidents@pvta.com>},
+    exception_recipients: %w[david.faulkenberry@gmail.com]
+  }
 end
