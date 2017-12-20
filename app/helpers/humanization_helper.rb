1df545469f3aaa191f4f3c085b7eafad97f4d793
David Faulkenberry <david.faulkenberry@gmail.com>
2017-12-20 16:05:19 -0500

    Use for incident reports

--- app/helpers/humanization_helper.rb
+++ app/helpers/humanization_helper.rb
@@ -1,7 +1,7 @@
 module HumanizationHelper
   def human_name(model, name)
     text = t name, scope: [:activerecord, :attributes, model]
-    if text.end_with? '?'
+    if text.include? '?'
       text
     else text + ':'
     end
