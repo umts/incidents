e6ddad565be418423c42dab72bcae68ba75f5b30
David Faulkenberry <david.faulkenberry@gmail.com>
2017-12-20 14:43:41 -0500

    Numbers don't have case

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,6 @@ Devise.setup do |config|
 
   config.mailer_sender = 'incidents@pvtaapps.com'
   config.authentication_keys = [:badge_number]
-  config.case_insensitive_keys = [:badge_number]
   config.strip_whitespace_keys = [:badge_number]
   config.stretches = Rails.env.test? ? 1 : 11
   config.timeout_in = 30.minutes
