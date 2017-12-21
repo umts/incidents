0aa3b3d0441857c4cd94e74e646e9cf98546bf9a
David Faulkenberry <david.faulkenberry@gmail.com>
2017-12-21 15:02:49 -0500

    Add RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'factory_bot_rails'
+require 'simplecov'
+
+SimpleCov.start 'rails'
+SimpleCov.start do
+  add_filter '/config/'
+  add_filter '/spec/'
+  refuse_coverage_drop
+end
+
+RSpec.configure do |config|
+  config.before :all do
+    FactoryBot.reload
+  end
+  config.include FactoryBot::Syntax::Methods
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.order = :random
+end
