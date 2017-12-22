af8a65eb3e301fb6694631d79200a47634e82a5e
David Faulkenberry <david.faulkenberry@gmail.com>
2017-12-22 14:11:19 -0500

    Creating incident reports as a supervisor

--- spec/system/supervisors/creating_incidents_spec.rb
+++ spec/system/supervisors/creating_incidents_spec.rb
@@ -27,4 +27,37 @@ describe 'creating incidents as a supervisor' do
     visit new_incident_url
     expect(page).to have_select 'Supervisor', options: ['', user.proper_name]
   end
+  it 'does not send you directly to fill in the report after creating it' do
+    visit new_incident_url
+    select driver_in_division.proper_name, from: 'Driver'
+    select user.proper_name, from: 'Supervisor'
+    click_on 'Create Incident Report'
+    expect(page.current_url).to end_with incidents_path
+  end
+  it 'tells you that the report has been created successfully' do
+    visit new_incident_url
+    select driver_in_division.proper_name, from: 'Driver'
+    select user.proper_name, from: 'Supervisor'
+    click_on 'Create Incident Report'
+    expect(page).to have_text 'Incident was successfully created.'
+  end
+  context 'without selecting a supervisor' do
+    it 'creates a supervisor-less report' do
+      visit new_incident_url
+      select driver_in_division.proper_name, from: 'Driver'
+      select '', from: 'Supervisor'
+      click_on 'Create Incident Report'
+      expect(page).to have_text 'Incident was successfully created.'
+    end
+  end
+  context 'without selecting a driver' do
+    it 'does not create an incident' do
+      visit new_incident_url
+      select '', from: 'Driver'
+      select user.proper_name, from: 'Supervisor'
+      click_on 'Create Incident Report'
+      expect(page).not_to have_text 'Incident was successfully created.'
+      expect(page).to have_text 'Driver must exist'
+    end
+  end
 end
