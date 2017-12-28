39d60f35cc7e6a5216cfbfdb54b9fc863965ea2d
David Faulkenberry <david.faulkenberry@gmail.com>
2017-12-28 07:50:25 -0500

    Testing access control

---
+++ spec/system/access_control_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'access control' do
+  it 'prevents drivers from accessing supervisor resources' do
+    when_current_user_is :driver
+    incident = create :incident
+    visit edit_supervisor_report_url(incident.supervisor_report)
+    expect(page).not_to have_text 'Editing Supervisor Report'
+    expect(page).to have_text 'You do not have permission to access this page.'
+  end
+  it 'prevents drivers from accessing staff resources' do
+    when_current_user_is :driver
+    visit incomplete_incidents_url
+    expect(page).not_to have_text 'Incomplete Incidents'
+    expect(page).to have_text 'You do not have permission to access this page.'
+  end
+end
