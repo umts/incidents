187961de124747ecdd32d81bf9d5722f1fc44d61
David Faulkenberry <david.faulkenberry@gmail.com>
2018-01-03 14:36:48 -0500

    Test viewing report history

---
+++ spec/system/staff/viewing_incident_report_history_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'viewing incident report history as staff' do
+  let(:staff) { create :user, :staff }
+  before(:each) { when_current_user_is staff }
+  let(:incident) { incident_in_divisions(staff.divisions) }
+  it 'allowing viewing incident history' do
+    visit incident_url(incident)
+    within('.driver-incident-report') { click_on 'View full history' }
+    expect(page.current_url).to end_with history_incident_report_path(incident.driver_incident_report)
+  end
+end
