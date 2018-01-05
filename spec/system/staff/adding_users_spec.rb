d54dec3d88f93b2a8a27acee8301e027cf2bf68d
David Faulkenberry <david.faulkenberry@gmail.com>
2018-01-04 21:51:26 -0500

    Adding and deactivating users

---
+++ spec/system/staff/adding_users_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'adding users as staff' do
+  let(:staff) { create :user, :staff }
+  before(:each) { when_current_user_is staff }
+  it 'allows adding users' do
+    visit users_url
+    click_on 'Add New User'
+    expect(page.current_url).to end_with new_user_path
+    expect(page).to have_selector 'h1', text: 'Add New User'
+  end
+  it 'requires first name, last name, badge number, and divisions' do
+    visit new_user_url
+    click_button 'Save user'
+    wait_for_ajax!
+    expect(page).to have_selector 'p',
+      text: 'This user has 4 missing values and so cannot be saved:'
+    expect(page).to have_text "First name can't be blank"
+    expect(page).to have_text "Last name can't be blank"
+    expect(page).to have_text "Badge number can't be blank"
+    expect(page).to have_text "Divisions can't be blank"
+  end
+  it 'accepts new users which have the required values' do
+    create :division, name: 'UMASS'
+    visit new_user_url
+    fill_in 'First name', with: 'Leopold'
+    fill_in 'Last name', with: 'Markowski'
+    fill_in 'Badge number', with: '4087'
+    select 'UMASS', from: 'Divisions'
+    click_button 'Save user'
+    expect(page).to have_selector 'p.notice',
+      text: 'User was successfully created.'
+    expect(page.current_url).to end_with users_path
+  end
+end
---
+++ spec/system/staff/deactivating_users_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'deactivating and reactivating users as staff' do
+  let(:staff) { create :user, :staff }
+  before(:each) { when_current_user_is staff }
+  it 'allows deactivating users' do
+    driver = create :user, :driver
+    visit users_url
+    click_button 'Drivers'
+    # Only the one driver should be shown.
+    expect(page).to have_text driver.last_name
+    expect(page).to have_selector 'button',
+      text: 'Deactivate', count: 1
+    click_button 'Deactivate'
+    expect(page).to have_selector 'p.notice',
+      text: 'User was deactivated successfully.'
+    # We should be on the main users page, but have only the current user.
+    expect(page.current_url).to end_with users_path
+    expect(page).to have_selector 'table.index tbody tr', count: 1
+  end
+  it 'allows reactivating users' do
+    create :user, :driver, active: false
+    visit users_url(inactive: true)
+    click_button 'Drivers'
+    expect(page).to have_selector 'button',
+      text: 'Reactivate', count: 1
+    click_button 'Reactivate'
+    expect(page).to have_selector 'p.notice',
+      text: 'User was reactivated successfully.'
+    # We should be on the main users page, and therefore see the current user
+    # and the newly reactivated driver.
+    expect(page.current_url).to end_with users_path
+    expect(page).to have_selector 'table.index tbody tr', count: 2
+  end
+end
