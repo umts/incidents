e46242c95cf144b52f21b06a94927579022b4bb8
David Faulkenberry <david.faulkenberry@gmail.com>
2018-02-01 12:55:24 -0500

    Update specs to match addition of root cause analysis

--- spec/system/staff/editing_incident_details_spec.rb
+++ spec/system/staff/editing_incident_details_spec.rb
@@ -15,18 +15,20 @@ describe 'editing incident details as staff' do
     expect(page).to have_selector 'p.notice',
       text: 'Incident report was successfully saved.'
   end
-  it 'requires reason codes for completed incidents' do
+  it 'requires reason codes and root cause analysis for completed incidents' do
     create :reason_code, identifier: 'A-1', description: 'Falling bananas'
     visit edit_incident_url(incident)
     check 'Completed'
     select '', from: 'Reason code'
     click_button 'Save incident details'
     wait_for_ajax!
-    expect(page).to have_text 'This incident has 2 missing values and so cannot be marked as completed.'
+    expect(page).to have_text 'This incident has 3 missing values and so cannot be marked as completed.'
     expect(page).to have_text "Reason code can't be blank"
     expect(page).to have_text "Second reason code can't be blank"
+    expect(page).to have_text "Root cause analysis can't be blank"
     select 'A-1: Falling bananas', from: 'Reason code'
     select 'a-8: Miscellaneous', from: 'Second reason code'
+    fill_in 'Root cause analysis', with: 'Lorem ipsum dolor sit amet.'
     click_button 'Save incident details'
     wait_for_ajax!
     expect(page).to have_selector 'p.notice',
