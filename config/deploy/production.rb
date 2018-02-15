9431d336c4736ab3ee920ff48a9b2b342d78bf28
David Faulkenberry <david.faulkenberry@gmail.com>
2018-02-15 13:44:00 -0500

    Space after comma

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
-remote_user = Net::SSH::Config.for('50.198.83.200')[:user] || ENV['USER']
-server '50.198.83.200', user: remote_user, roles: %w(app db web), port: 2250
+config = Net::SSH::Config.for 'incidents.pvta.com'
+remote_user = config[:user] || ENV[:user]
+port = config[:port] || ENV[:port]
+server 'incidents.pvta.com', user: remote_user, roles: %w(app db web), port: port
 set :tmp_dir, "/tmp/#{remote_user}"
 set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/home/umass/passenger_temp' }
