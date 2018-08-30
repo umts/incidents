5e87361697c1ce7e9c81d93c673026ef4c08b837
Molly O'Neil <mboneil@umass.edu>
2018-08-30 12:19:33 -0400

    Editing tests descriptions for drivers

---
+++ spec/system/drivers/editing_incidents_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'editing incidents as a driver' do
+  it 'allows editing reports' do
+  end
+  context 'admin deletes the incident' do
+    it 'displays a nice error message' do
+    end
+  end
+end
