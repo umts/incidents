ce8bea2380b184e7e8502d384b93340284c37bb5
Molly O'Neil <mboneil@umass.edu>
2018-08-31 13:46:31 -0400

    Remove dup tests (already in create), fill out issue scenario

--- spec/system/drivers/editing_incidents_spec.rb
+++ spec/system/drivers/editing_incidents_spec.rb
@@ -3,10 +3,17 @@
 require 'spec_helper'
 
 describe 'editing incidents as a driver' do
-  it 'allows editing reports' do
-  end
+  let(:driver) { create :user, :driver}
+  before(:each){ when_current_user_is driver }
+  let(:report) { create :incident_report, user: driver }
+  let(:incident) { create :incident, incident_report: report }
   context 'admin deletes the incident' do
     it 'displays a nice error message' do
+      visit edit_incident_url(incident)
+      incident.destroy
+      click_button 'Save report'
+      wait_for_ajax!
+      expect(response.status).to eq 500
     end
   end
 end
