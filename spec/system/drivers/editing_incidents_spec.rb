adaa8e3beb9ca20b91cbf7eb5eab213090a33ed5
Molly O'Neil <mboneil@umass.edu>
2018-08-31 14:11:28 -0400

    Finish the last scenario for edit, admin delete, save

--- spec/system/drivers/editing_incidents_spec.rb
+++ spec/system/drivers/editing_incidents_spec.rb
@@ -3,13 +3,17 @@
 require 'spec_helper'
 
 describe 'editing incidents as a driver' do
-  let(:driver) { create :user, :driver}
-  before(:each){ when_current_user_is driver }
-  let(:report) { create :incident_report, user: driver }
-  let(:incident) { create :incident, incident_report: report }
+  let(:driver) { create :user, :driver }
+  before(:each) { when_current_user_is driver }
+  let(:report) { create :incident_report, :driver_report, user: driver }
+  let!(:incident) { create :incident, driver_incident_report: report }
   context 'admin deletes the incident' do
     it 'displays a nice error message' do
-      visit edit_incident_url(incident)
+      visit incidents_url
+      expect(page).to have_selector 'table.incidents tbody tr', count: 1
+      within 'tr', text: driver.proper_name do
+        click_button 'Edit'
+      end
       incident.destroy
       click_button 'Save report'
       wait_for_ajax!
