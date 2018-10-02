7499ddb06bf3ef0f7c1b558e52f444261766e921
Molly O'Neil <mboneil@umass.edu>
2018-10-02 10:49:58 -0400

    Add section to before each

--- spec/system/drivers/editing_incidents_spec.rb
+++ spec/system/drivers/editing_incidents_spec.rb
@@ -7,13 +7,15 @@ describe 'editing incidents as a driver' do
   before(:each) { when_current_user_is driver }
   let(:report) { create :incident_report, :driver_report, user: driver }
   let!(:incident) { create :incident, driver_incident_report: report }
+  before :each do
+    visit incidents_url
+    expect(page).to have_selector 'table.incidents tbody tr', count: 1
+    within 'tr', text: driver.proper_name do
+      click_button 'Edit'
+    end
+  end
   context 'admin deletes the incident' do
     it 'displays a nice error message' do
-      visit incidents_url
-      expect(page).to have_selector 'table.incidents tbody tr', count: 1
-      within 'tr', text: driver.proper_name do
-        click_button 'Edit'
-      end
       expect(page).to have_content 'Editing Driver Account of Incident'
       incident.destroy
       click_button 'Save report'
