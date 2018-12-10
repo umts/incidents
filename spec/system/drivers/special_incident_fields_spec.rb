9ab8a3f7137df7e5b8ac5a10f31a43ffcf54a932
Molly O'Neil <mboneil@umass.edu>
2018-12-10 15:36:41 -0500

    Abide to rubocop and some before eachs

--- spec/system/drivers/special_incident_fields_spec.rb
+++ spec/system/drivers/special_incident_fields_spec.rb
@@ -7,15 +7,16 @@ describe 'special incident fields' do
   before(:each) { when_current_user_is driver }
   describe 'collision related fields' do
     context 'without collision' do
-      it 'allows filling in collision fields' do
+      before :each do 
         visit new_incident_path
+      end
+      it 'allows filling in collision fields' do
         expect(page).not_to have_text 'Motor Vehicle Collision Information'
         check 'Did the incident involve a collision?'
         wait_for_animation!
         expect(page).to have_text 'Motor Vehicle Collision Information'
       end
       it 'does not require filling in fields for collisions' do
-        visit new_incident_path
         fill_in_base_incident_fields
         check 'Did the incident involve a collision?'
         wait_for_animation!
@@ -25,8 +26,11 @@ describe 'special incident fields' do
     end
     context 'with collision' do
       it 'shows collision fields for collision incidents' do
-        driver_report = create :incident_report, :driver_report, :with_incident,
-          :collision, user: driver
+        driver_report = create :incident_report,
+                               :driver_report,
+                               :with_incident,
+                               :collision,
+                               user: driver
         create :incident, driver_incident_report: driver_report
         expect(driver_report).to be_motor_vehicle_collision
         visit edit_incident_report_url(driver_report)
@@ -49,8 +53,12 @@ describe 'special incident fields' do
     end
     context 'with other vehicle info' do
       it 'shows other vehicle owner information as necessary' do
-        driver_report = create :incident_report, :driver_report, :with_incident,
-          :collision, :other_vehicle_not_driven_by_owner, user: driver
+        driver_report = create :incident_report,
+                               :driver_report,
+                               :with_incident,
+                               :collision,
+                               :other_vehicle_not_driven_by_owner,
+                               user: driver
         create :incident, driver_incident_report: driver_report
         expect(driver_report).not_to be_other_vehicle_owned_by_other_driver
         visit edit_incident_report_url(driver_report)
@@ -61,8 +69,10 @@ describe 'special incident fields' do
 
   describe 'police related fields' do
     context 'without police info' do
-      it 'allows filling in police info' do
+      before :each do
         visit new_incident_path
+      end
+      it 'allows filling in police info' do
         check 'Did the incident involve a collision?'
         wait_for_animation!
         expect(page).not_to have_field 'Police badge number'
@@ -71,7 +81,6 @@ describe 'special incident fields' do
         expect(page).to have_field 'Police badge number'
       end
       it 'does not require police info' do
-        visit new_incident_path
         fill_in_base_incident_fields
         check 'Did the incident involve a collision?'
         wait_for_animation!
@@ -83,8 +92,12 @@ describe 'special incident fields' do
     end
     context 'with police info' do
       it 'shows police fields as necessary' do
-        driver_report = create :incident_report, :driver_report, :with_incident,
-          :collision, :police_on_scene, user: driver
+        driver_report = create :incident_report,
+                               :driver_report,
+                               :with_incident,
+                               :collision,
+                               :police_on_scene,
+                               user: driver
         create :incident, driver_incident_report: driver_report
         expect(driver_report).to be_police_on_scene
         visit edit_incident_report_url(driver_report)
@@ -95,15 +108,16 @@ describe 'special incident fields' do
 
   describe 'passenger incident related fields' do
     context 'without passenger incidents' do
-      it 'allows filling in passenger incident fields' do
+      before :each do
         visit new_incident_path
+      end
+      it 'allows filling in passenger incident fields' do
         expect(page).not_to have_text 'Passenger Incident Information'
         check 'Did the incident involve a passenger?'
         wait_for_animation!
         expect(page).to have_text 'Passenger Incident Information'
       end
       it 'does not require filling in passenger incident fields' do
-        visit new_incident_path
         fill_in_base_incident_fields
         check 'Did the incident involve a passenger?'
         wait_for_animation!
@@ -114,8 +128,11 @@ describe 'special incident fields' do
     end
     context 'with passenger incidents' do
       it 'shows passenger fields for passenger incidents' do
-        driver_report = create :incident_report, :driver_report, :with_incident, 
-          :passenger_incident, user: driver
+        driver_report = create :incident_report,
+                               :driver_report,
+                               :with_incident,
+                               :passenger_incident,
+                               user: driver
         expect(driver_report).to be_passenger_incident
         visit edit_incident_report_url(driver_report)
         expect(page).to have_text 'Passenger Incident Information'
@@ -124,9 +141,11 @@ describe 'special incident fields' do
   end
 
   describe 'reason not up to curb related fields' do
+    before :each do
+      visit new_incident_path
+    end
     context 'without reason not up to curb' do
       it 'allows filling in reason bus was not up to curb' do
-        visit new_incident_path
         check 'Did the incident involve a passenger?'
         wait_for_animation!
         expect(page).not_to have_field 'Reason not up to curb'
@@ -136,7 +155,6 @@ describe 'special incident fields' do
         expect(page).to have_field 'Reason not up to curb'
       end
       it 'does not require filling in reason not up to curb' do
-        visit new_incident_path
         fill_in_base_incident_fields
         check 'Did the incident involve a passenger?'
         wait_for_animation!
@@ -149,8 +167,12 @@ describe 'special incident fields' do
     end
     context 'with reason not up to curb' do
       it 'shows reason not up to curb field as necessary' do
-        driver_report = create :incident_report, :driver_report, :with_incident,
-          :passenger_incident, :not_up_to_curb, user: driver
+        driver_report = create :incident_report,
+                               :driver_report,
+                               :with_incident,
+                               :passenger_incident,
+                               :not_up_to_curb,
+                               user: driver
         expect(driver_report).to be_needs_reason_not_up_to_curb
         visit edit_incident_report_url(driver_report)
         expect(page).to have_field 'Reason not up to curb'
