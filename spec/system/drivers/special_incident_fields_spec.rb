bb857c28aaddfbb35b7616be371287bc5da8fef7
Molly O'Neil <mboneil@umass.edu>
2018-12-10 15:30:21 -0500

    Add in context for clarity

--- spec/system/drivers/special_incident_fields_spec.rb
+++ spec/system/drivers/special_incident_fields_spec.rb
@@ -6,135 +6,155 @@ describe 'special incident fields' do
   let(:driver) { create :user, :driver }
   before(:each) { when_current_user_is driver }
   describe 'collision related fields' do
-    it 'allows filling in collision fields' do
-      visit new_incident_path
-      expect(page).not_to have_text 'Motor Vehicle Collision Information'
-      check 'Did the incident involve a collision?'
-      wait_for_animation!
-      expect(page).to have_text 'Motor Vehicle Collision Information'
+    context 'without collision' do
+      it 'allows filling in collision fields' do
+        visit new_incident_path
+        expect(page).not_to have_text 'Motor Vehicle Collision Information'
+        check 'Did the incident involve a collision?'
+        wait_for_animation!
+        expect(page).to have_text 'Motor Vehicle Collision Information'
+      end
+      it 'does not require filling in fields for collisions' do
+        visit new_incident_path
+        fill_in_base_incident_fields
+        check 'Did the incident involve a collision?'
+        wait_for_animation!
+        click_on 'Save report'
+        expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
+      end
     end
-    it 'does not require filling in fields for collisions' do
-      visit new_incident_path
-      fill_in_base_incident_fields
-      check 'Did the incident involve a collision?'
-      wait_for_animation!
-      click_on 'Save report'
-      expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
-    end
-    it 'shows collision fields for collision incidents' do
-      driver_report = create :incident_report, :driver_report, :with_incident,
-        :collision, user: driver
-      create :incident, driver_incident_report: driver_report
-      expect(driver_report).to be_motor_vehicle_collision
-      visit edit_incident_report_url(driver_report)
-      expect(page).to have_text 'Motor Vehicle Collision Information'
+    context 'with collision' do
+      it 'shows collision fields for collision incidents' do
+        driver_report = create :incident_report, :driver_report, :with_incident,
+          :collision, user: driver
+        create :incident, driver_incident_report: driver_report
+        expect(driver_report).to be_motor_vehicle_collision
+        visit edit_incident_report_url(driver_report)
+        expect(page).to have_text 'Motor Vehicle Collision Information'
+      end
     end
   end
 
   describe 'other vehicle related fields' do
-    it 'allows filling in other vehicle info as necessary' do
-      visit new_incident_path
-      check 'Did the incident involve a collision?'
-      wait_for_animation!
-      expect(page).to have_field 'Other vehicle owner name'
-      check 'Is the other driver involved the owner of the vehicle?'
-      wait_for_animation!
-      expect(page).not_to have_field 'Other vehicle owner name'
+    context 'without other vehicle info' do
+      it 'allows filling in other vehicle info as necessary' do
+        visit new_incident_path
+        check 'Did the incident involve a collision?'
+        wait_for_animation!
+        expect(page).to have_field 'Other vehicle owner name'
+        check 'Is the other driver involved the owner of the vehicle?'
+        wait_for_animation!
+        expect(page).not_to have_field 'Other vehicle owner name'
+      end
     end
-    it 'shows other vehicle owner information as necessary' do
-      driver_report = create :incident_report, :driver_report, :with_incident,
-        :collision, :other_vehicle_not_driven_by_owner, user: driver
-      create :incident, driver_incident_report: driver_report
-      expect(driver_report).not_to be_other_vehicle_owned_by_other_driver
-      visit edit_incident_report_url(driver_report)
-      expect(page).to have_field 'Other vehicle owner name'
+    context 'with other vehicle info' dp
+      it 'shows other vehicle owner information as necessary' do
+        driver_report = create :incident_report, :driver_report, :with_incident,
+          :collision, :other_vehicle_not_driven_by_owner, user: driver
+        create :incident, driver_incident_report: driver_report
+        expect(driver_report).not_to be_other_vehicle_owned_by_other_driver
+        visit edit_incident_report_url(driver_report)
+        expect(page).to have_field 'Other vehicle owner name'
+      end
     end
   end
 
   describe 'police related fields' do
-    it 'allows filling in police info' do
-      visit new_incident_path
-      check 'Did the incident involve a collision?'
-      wait_for_animation!
-      expect(page).not_to have_field 'Police badge number'
-      check 'Did police respond to the incident?'
-      wait_for_animation!
-      expect(page).to have_field 'Police badge number'
+    context 'without police info' do
+      it 'allows filling in police info' do
+        visit new_incident_path
+        check 'Did the incident involve a collision?'
+        wait_for_animation!
+        expect(page).not_to have_field 'Police badge number'
+        check 'Did police respond to the incident?'
+        wait_for_animation!
+        expect(page).to have_field 'Police badge number'
+      end
+      it 'does not require police info' do
+        visit new_incident_path
+        fill_in_base_incident_fields
+        check 'Did the incident involve a collision?'
+        wait_for_animation!
+        check 'Did police respond to the incident?'
+        wait_for_animation!
+        click_on 'Save report'
+        expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
+      end
     end
-    it 'does not require police info' do
-      visit new_incident_path
-      fill_in_base_incident_fields
-      check 'Did the incident involve a collision?'
-      wait_for_animation!
-      check 'Did police respond to the incident?'
-      wait_for_animation!
-      click_on 'Save report'
-      expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
-    end
-    it 'shows police fields as necessary' do
-      driver_report = create :incident_report, :driver_report, :with_incident,
-        :collision, :police_on_scene, user: driver
-      create :incident, driver_incident_report: driver_report
-      expect(driver_report).to be_police_on_scene
-      visit edit_incident_report_url(driver_report)
-      expect(page).to have_field 'Police badge number'
+    context 'with police info' do
+      it 'shows police fields as necessary' do
+        driver_report = create :incident_report, :driver_report, :with_incident,
+          :collision, :police_on_scene, user: driver
+        create :incident, driver_incident_report: driver_report
+        expect(driver_report).to be_police_on_scene
+        visit edit_incident_report_url(driver_report)
+        expect(page).to have_field 'Police badge number'
+      end
     end
   end
 
   describe 'passenger incident related fields' do
-    it 'allows filling in passenger incident fields' do
-      visit new_incident_path
-      expect(page).not_to have_text 'Passenger Incident Information'
-      check 'Did the incident involve a passenger?'
-      wait_for_animation!
-      expect(page).to have_text 'Passenger Incident Information'
+    context 'without passenger incidents' do
+      it 'allows filling in passenger incident fields' do
+        visit new_incident_path
+        expect(page).not_to have_text 'Passenger Incident Information'
+        check 'Did the incident involve a passenger?'
+        wait_for_animation!
+        expect(page).to have_text 'Passenger Incident Information'
+      end
+      it 'does not require filling in passenger incident fields' do
+        visit new_incident_path
+        fill_in_base_incident_fields
+        check 'Did the incident involve a passenger?'
+        wait_for_animation!
+        expect(page).to have_text 'Passenger Incident Information'
+        click_on 'Save report'
+        expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
+      end
     end
-    it 'does not require filling in passenger incident fields' do
-      visit new_incident_path
-      fill_in_base_incident_fields
-      check 'Did the incident involve a passenger?'
-      wait_for_animation!
-      expect(page).to have_text 'Passenger Incident Information'
-      click_on 'Save report'
-      expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
-    end
-    it 'shows passenger fields for passenger incidents' do
-      driver_report = create :incident_report, :driver_report, :with_incident, 
-        :passenger_incident, user: driver
-      expect(driver_report).to be_passenger_incident
-      visit edit_incident_report_url(driver_report)
-      expect(page).to have_text 'Passenger Incident Information'
+    context 'with passenger incidents' do
+      it 'shows passenger fields for passenger incidents' do
+        driver_report = create :incident_report, :driver_report, :with_incident, 
+          :passenger_incident, user: driver
+        expect(driver_report).to be_passenger_incident
+        visit edit_incident_report_url(driver_report)
+        expect(page).to have_text 'Passenger Incident Information'
+      end
     end
   end
 
-  describe 'curb related fields' do
-    it 'allows filling in reason bus was not up to curb' do
-      visit new_incident_path
-      check 'Did the incident involve a passenger?'
-      wait_for_animation!
-      expect(page).not_to have_field 'Reason not up to curb'
-      select 'Stopped', from: 'Motion of bus'
-      uncheck 'Was the bus pulled completely up to the curb?'
-      wait_for_animation!
-      expect(page).to have_field 'Reason not up to curb'
+  describe 'reason not up to curb related fields' do
+    context 'without reason not up to curb' do
+      it 'allows filling in reason bus was not up to curb' do
+        visit new_incident_path
+        check 'Did the incident involve a passenger?'
+        wait_for_animation!
+        expect(page).not_to have_field 'Reason not up to curb'
+        select 'Stopped', from: 'Motion of bus'
+        uncheck 'Was the bus pulled completely up to the curb?'
+        wait_for_animation!
+        expect(page).to have_field 'Reason not up to curb'
+      end
+      it 'does not require filling in reason not up to curb' do
+        visit new_incident_path
+        fill_in_base_incident_fields
+        check 'Did the incident involve a passenger?'
+        wait_for_animation!
+        select 'Stopped', from: 'Motion of bus'
+        uncheck 'Was the bus pulled completely up to the curb?'
+        wait_for_animation!
+        click_on 'Save report'
+        expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
+      end
     end
-    it 'does not require filling in reason not up to curb' do
-      visit new_incident_path
-      fill_in_base_incident_fields
-      check 'Did the incident involve a passenger?'
-      wait_for_animation!
-      select 'Stopped', from: 'Motion of bus'
-      uncheck 'Was the bus pulled completely up to the curb?'
-      wait_for_animation!
-      click_on 'Save report'
-      expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
-    end
-    it 'shows reason not up to curb field as necessary' do
-      driver_report = create :incident_report, :driver_report, :with_incident,
-        :passenger_incident, :not_up_to_curb, user: driver
-      expect(driver_report).to be_needs_reason_not_up_to_curb
-      visit edit_incident_report_url(driver_report)
-      expect(page).to have_field 'Reason not up to curb'
+    context 'with reason not up to curb' do
+      it 'shows reason not up to curb field as necessary' do
+        driver_report = create :incident_report, :driver_report, :with_incident,
+          :passenger_incident, :not_up_to_curb, user: driver
+        expect(driver_report).to be_needs_reason_not_up_to_curb
+        visit edit_incident_report_url(driver_report)
+        expect(page).to have_field 'Reason not up to curb'
+      end
     end
   end
 end
