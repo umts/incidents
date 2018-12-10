0173e1e873f186a2b5ccac6b4ef0251870b5c7c4
Molly O'Neil <mboneil@umass.edu>
2018-12-10 15:17:58 -0500

    Wait for animation after each checkbox is checked

--- spec/system/drivers/special_incident_fields_spec.rb
+++ spec/system/drivers/special_incident_fields_spec.rb
@@ -9,6 +9,7 @@ describe 'special incident fields' do
     visit new_incident_path
     expect(page).not_to have_text 'Motor Vehicle Collision Information'
     check 'Did the incident involve a collision?'
+    wait_for_animation!
     expect(page).to have_text 'Motor Vehicle Collision Information'
   end
   it 'does not require filling in fields for collisions' do
@@ -34,6 +35,7 @@ describe 'special incident fields' do
     wait_for_animation!
     expect(page).to have_field 'Other vehicle owner name'
     check 'Is the other driver involved the owner of the vehicle?'
+    wait_for_animation!
     expect(page).not_to have_field 'Other vehicle owner name'
   end
   it 'shows other vehicle owner information as necessary' do
@@ -60,6 +62,7 @@ describe 'special incident fields' do
     check 'Did the incident involve a collision?'
     wait_for_animation!
     check 'Did police respond to the incident?'
+    wait_for_animation!
     click_on 'Save report'
     expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
   end
@@ -76,6 +79,7 @@ describe 'special incident fields' do
     visit new_incident_path
     expect(page).not_to have_text 'Passenger Incident Information'
     check 'Did the incident involve a passenger?'
+    wait_for_animation!
     expect(page).to have_text 'Passenger Incident Information'
   end
   it 'does not require filling in passenger incident fields' do
@@ -98,17 +102,21 @@ describe 'special incident fields' do
   it 'allows filling in reason bus was not up to curb' do
     visit new_incident_path
     check 'Did the incident involve a passenger?'
+    wait_for_animation!
     expect(page).not_to have_field 'Reason not up to curb'
     select 'Stopped', from: 'Motion of bus'
     uncheck 'Was the bus pulled completely up to the curb?'
+    wait_for_animation!
     expect(page).to have_field 'Reason not up to curb'
   end
   it 'does not require filling in reason not up to curb' do
     visit new_incident_path
     fill_in_base_incident_fields
     check 'Did the incident involve a passenger?'
+    wait_for_animation!
     select 'Stopped', from: 'Motion of bus'
     uncheck 'Was the bus pulled completely up to the curb?'
+    wait_for_animation!
     click_on 'Save report'
     expect(page.current_url).to end_with incident_path(Incident.last, format: :pdf)
   end
