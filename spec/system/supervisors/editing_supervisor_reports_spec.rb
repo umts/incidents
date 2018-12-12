9649571821d3d5201ff14f469a507b90ba38e535
Molly O'Neil <mboneil@umass.edu>
2018-12-12 10:00:51 -0500

    Line length issues

--- spec/system/supervisors/editing_supervisor_reports_spec.rb
+++ spec/system/supervisors/editing_supervisor_reports_spec.rb
@@ -9,13 +9,15 @@ describe 'editing supervisor reports as a supervisor' do
   it 'allows editing supervisor reports' do
     visit edit_incident_url(incident)
     click_on 'Edit Supervisor Report'
-    expect(page.current_url).to end_with edit_supervisor_report_path(incident.supervisor_report)
+    expect(page.current_url)
+      .to end_with edit_supervisor_report_path(incident.supervisor_report)
     check 'Were pictures taken?'
     wait_for_animation!
     fill_in 'Number of pictures saved', with: 37
     click_button 'Save supervisor report'
     wait_for_ajax!
-    expect(page).to have_selector 'p.notice', text: 'Incident report was successfully saved.'
+    expect(page).to have_selector 'p.notice',
+                                  text: 'Incident report was successfully saved.'
     expect(page).to have_text 'Number of pictures saved: 37'
   end
   context 'admin deletes the incident' do
@@ -26,7 +28,8 @@ describe 'editing supervisor reports as a supervisor' do
       incident.destroy
       click_button 'Save supervisor report'
       wait_for_ajax!
-      expect(page).to have_selector 'p.notice', text: 'This incident report no longer exists.'
+      expect(page).to have_selector 'p.notice',
+                                    text: 'This incident report no longer exists.'
     end
   end
   # this test fails because the second_field is disabled...?
@@ -43,8 +46,10 @@ describe 'editing supervisor reports as a supervisor' do
       fill_in 'Name', with: 'Adam'
       fill_in 'Address', with: '255 Governors Dr, Amherst'
       click_button 'Add witness info'
-      fill_in 'supervisor_report_witnesses_attributes_1_name', with: 'Karin'
-      fill_in 'supervisor_report_witnesses_attributes_1_address', with: '51 Forestry Way, Amherst'
+      fill_in 'supervisor_report_witnesses_attributes_1_name',
+              with: 'Karin'
+      fill_in 'supervisor_report_witnesses_attributes_1_address',
+              with: '51 Forestry Way, Amherst'
       click_button 'Save supervisor report'
 
       visit incident_url(incident)
