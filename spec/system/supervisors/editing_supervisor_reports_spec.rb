e10e6a4d1e668147bcd438a32668331882ba7f52
tanvish <tanvishjha@gmail.com>
2019-09-25 16:03:10 -0400

    Fixed some failing tests.

--- spec/system/supervisors/editing_supervisor_reports_spec.rb
+++ spec/system/supervisors/editing_supervisor_reports_spec.rb
@@ -25,7 +25,7 @@ describe 'editing supervisor reports as a supervisor' do
 
     visit edit_incident_path(incident)
     click_on 'Edit Supervisor Report'
-    check 'I can completely discount the operator' # But don't give a reason
+    select 'Post Accident: Threshold met and discounted' # But don't give a reason
 
     click_button 'Save supervisor report'
     wait_for_ajax!
