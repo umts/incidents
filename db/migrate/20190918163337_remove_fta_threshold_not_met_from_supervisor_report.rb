4f4900e62eecd9ad1d7a45d715862c8d66214aef
tanvish <tanvishjha@gmail.com>
2019-10-23 11:08:56 -0400

    Updating test status for discounted drivers and when threshold not met and then removing the respective columns from the table because we no longer need them.

--- db/migrate/20190918163337_remove_fta_threshold_not_met_from_supervisor_report.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFtaThresholdNotMetFromSupervisorReport < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :supervisor_reports, :fta_threshold_not_met, :boolean
-  end
-end
--- db/migrate/20190918163407_remove_driver_discounted_from_supervisor_report.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDriverDiscountedFromSupervisorReport < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :supervisor_reports, :driver_discounted, :boolean
-  end
-end
