c200ec61a8a3ea9b4a6c61dcd2d0163e90cb237e
tanvish <tanvishjha@gmail.com>
2019-11-06 16:16:42 -0500

    This is an attempt at fixing the FactoryBot issue. The tests are passing successfully.

--- spec/factories/supervisor_reports.rb
+++ spec/factories/supervisor_reports.rb
@@ -5,8 +5,6 @@ FactoryBot.define do
     pictures_saved { [true, false].sample }
     passenger_statement { FFaker::Lorem.paragraphs(5).join "\n" }
     faxed { [nil, Time.zone.now].sample }
-    test_status 'Post Accident: Threshold met and discounted (no drug test)'
-    reason_driver_discounted 'Post-Accident: Threshold met (completed drug test)'
     testing_facility { SupervisorReport::TESTING_FACILITIES.sample }
     testing_facility_notified_at { Time.zone.now - 6.minutes }
     employee_notified_of_test_at { Time.zone.now - 5.minutes }
@@ -24,23 +22,6 @@ FactoryBot.define do
 
       rand(3).times do
         create :witness, supervisor_report: report
-      end
-      if report.test_status.include?('Post-Accident: Threshold met (completed drug test)')
-        field = %w[
-          test_due_to_bodily_injury
-          test_due_to_disabling_damage
-          test_due_to_fatality
-        ].sample
-        report.send field + '=', true
-      elsif report.test_status.include?('Reasonable Suspicion: Completed drug test')
-        report.observation_made_at = Time.zone.now - 5.minutes
-        reason = %w[appearance behavior speech odor].sample
-        report.send "test_due_to_employee_#{reason}=", true
-        report.send "employee_#{reason}=", FFaker::Lorem.sentence
-      elsif report.fta_threshold_not_met?
-        report.reason_threshold_not_met = FFaker::Lorem.sentence
-      else
-        report.reason_driver_discounted = FFaker::Lorem.sentence
       end
     end
   end
