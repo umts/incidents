9319244648708b9869e335597310087f46ef0045
Matt Moretti <werebus@gmail.com>
2020-04-03 12:23:45 -0400

    Rails upgrade task run

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,10 @@ module Incidents
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.autoload_paths << Rails.root.join('lib', 'extensions')
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
