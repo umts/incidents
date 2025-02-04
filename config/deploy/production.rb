7e388fb6adac318ff29d458705bf56d2f36c25ee
Matt Moretti <werebus@gmail.com>
2025-02-04 14:28:59 -0500

    Update Capistrano config for new capistrano-sidekiq

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 server 'incidents.pvta.com',
-       roles: %w[app db web],
+       roles: %w[app db web worker],
        ssh_options: { forward_agent: false }
 
+set :sidekiq_service_unit_name, 'sidekiq'
 set :passenger_restart_with_touch, true
