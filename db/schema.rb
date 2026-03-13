d57a167d2bba746f2e72ede0e10328def62af209
Matt Moretti <werebus@gmail.com>
2026-03-13 16:00:37 -0400

    Re-dump schema (#796)
    Rails 8.1 has alphabetized schema dumps. Doing this ahead of writing a
    migration for something else.

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_30_192451) do
+ActiveRecord::Schema[8.1].define(version: 2024_09_30_192451) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", precision: nil, null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", precision: nil, null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -40,10 +40,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_30_192451) do
   end
 
   create_table "divisions", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
     t.integer "claims_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.string "name", null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "divisions_users", id: false, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
@@ -53,236 +53,236 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_30_192451) do
   end
 
   create_table "incident_reports", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "run"
-    t.string "block"
-    t.string "bus"
-    t.integer "passengers_onboard"
-    t.integer "courtesy_cards_distributed"
-    t.integer "courtesy_cards_collected"
-    t.integer "speed"
-    t.string "location"
-    t.string "town"
-    t.string "weather_conditions"
-    t.string "road_conditions"
-    t.string "light_conditions"
-    t.boolean "headlights_used", default: false
-    t.boolean "motor_vehicle_collision", default: false
-    t.string "other_vehicle_plate"
-    t.string "other_vehicle_state"
-    t.string "other_vehicle_make"
-    t.string "other_vehicle_model"
-    t.string "other_vehicle_year"
-    t.string "other_vehicle_color"
-    t.integer "other_vehicle_passengers"
-    t.string "direction"
-    t.string "other_vehicle_direction"
-    t.string "other_driver_name"
-    t.string "other_driver_license_number"
-    t.string "other_driver_license_state"
-    t.string "other_vehicle_driver_address"
-    t.string "other_vehicle_driver_address_town"
-    t.string "other_vehicle_driver_address_state"
-    t.string "other_vehicle_driver_address_zip"
-    t.string "other_vehicle_driver_home_phone"
-    t.string "other_vehicle_driver_cell_phone"
-    t.string "other_vehicle_driver_work_phone"
-    t.boolean "other_vehicle_owned_by_other_driver", default: false
-    t.string "other_vehicle_owner_name"
-    t.string "other_vehicle_owner_address"
-    t.string "other_vehicle_owner_address_town"
-    t.string "other_vehicle_owner_address_state"
-    t.string "other_vehicle_owner_address_zip"
-    t.string "other_vehicle_owner_home_phone"
-    t.string "other_vehicle_owner_cell_phone"
-    t.string "other_vehicle_owner_work_phone"
-    t.boolean "police_on_scene", default: false
-    t.string "police_badge_number"
-    t.string "police_town_or_state"
-    t.string "police_case_assigned"
-    t.string "damage_to_bus_point_of_impact"
-    t.string "damage_to_other_vehicle_point_of_impact"
-    t.string "insurance_carrier"
-    t.string "insurance_policy_number"
-    t.date "insurance_effective_date"
-    t.boolean "passenger_incident", default: false
-    t.boolean "occurred_front_door", default: false
-    t.boolean "occurred_rear_door", default: false
-    t.boolean "occurred_front_steps", default: false
-    t.boolean "occurred_rear_steps", default: false
-    t.boolean "occurred_sudden_stop", default: false
-    t.boolean "occurred_before_boarding", default: false
-    t.boolean "occurred_while_boarding", default: false
-    t.boolean "occurred_after_boarding", default: false
-    t.boolean "occurred_while_exiting", default: false
-    t.boolean "occurred_after_exiting", default: false
-    t.string "motion_of_bus"
-    t.string "condition_of_steps"
-    t.boolean "bus_kneeled", default: false
-    t.boolean "bus_up_to_curb", default: false
-    t.string "reason_not_up_to_curb"
-    t.string "vehicle_in_bus_stop_plate"
-    t.text "description"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.string "point_of_impact"
-    t.integer "user_id"
-    t.string "surface_type"
-    t.string "surface_grade"
-    t.integer "posted_speed_limit"
-    t.boolean "credentials_exchanged", default: false
-    t.boolean "summons_or_warning_issued", default: false
-    t.text "summons_or_warning_info"
-    t.integer "bus_distance_from_curb"
-    t.datetime "occurred_at", precision: nil
-    t.boolean "towed_from_scene"
-    t.boolean "other_vehicle_towed_from_scene"
-    t.string "zip"
-    t.string "route"
-    t.text "vehicle_distance"
-    t.boolean "wheelchair_involved", default: false
-    t.boolean "other_passenger_information_taken", default: false
-    t.boolean "property_owner_information_taken", default: false
-    t.string "state"
     t.boolean "ambulance_present", default: false
     t.boolean "assistance_requested", default: false, null: false
+    t.string "block"
+    t.string "bus"
+    t.integer "bus_distance_from_curb"
+    t.boolean "bus_kneeled", default: false
+    t.boolean "bus_up_to_curb", default: false
     t.boolean "chair_on_lift", default: false, null: false
-    t.boolean "lift_deployed", default: false, null: false
+    t.string "condition_of_steps"
+    t.integer "courtesy_cards_collected"
+    t.integer "courtesy_cards_distributed"
+    t.datetime "created_at", precision: nil, null: false
+    t.boolean "credentials_exchanged", default: false
+    t.string "damage_to_bus_point_of_impact"
+    t.string "damage_to_other_vehicle_point_of_impact"
+    t.text "description"
+    t.string "direction"
+    t.boolean "headlights_used", default: false
     t.boolean "incident_involved_a_van", default: false, null: false
+    t.string "insurance_carrier"
+    t.date "insurance_effective_date"
+    t.string "insurance_policy_number"
+    t.boolean "lift_deployed", default: false, null: false
+    t.string "light_conditions"
+    t.string "location"
+    t.string "motion_of_bus"
+    t.boolean "motor_vehicle_collision", default: false
+    t.boolean "occurred_after_boarding", default: false
+    t.boolean "occurred_after_exiting", default: false
+    t.datetime "occurred_at", precision: nil
+    t.boolean "occurred_before_boarding", default: false
+    t.boolean "occurred_front_door", default: false
+    t.boolean "occurred_front_steps", default: false
+    t.boolean "occurred_rear_door", default: false
+    t.boolean "occurred_rear_steps", default: false
+    t.boolean "occurred_sudden_stop", default: false
+    t.boolean "occurred_while_boarding", default: false
+    t.boolean "occurred_while_exiting", default: false
+    t.string "other_driver_license_number"
+    t.string "other_driver_license_state"
+    t.string "other_driver_name"
+    t.boolean "other_passenger_information_taken", default: false
+    t.string "other_vehicle_color"
+    t.string "other_vehicle_direction"
+    t.string "other_vehicle_driver_address"
+    t.string "other_vehicle_driver_address_state"
+    t.string "other_vehicle_driver_address_town"
+    t.string "other_vehicle_driver_address_zip"
+    t.string "other_vehicle_driver_cell_phone"
+    t.string "other_vehicle_driver_home_phone"
+    t.string "other_vehicle_driver_work_phone"
+    t.string "other_vehicle_make"
+    t.string "other_vehicle_model"
+    t.boolean "other_vehicle_owned_by_other_driver", default: false
+    t.string "other_vehicle_owner_address"
+    t.string "other_vehicle_owner_address_state"
+    t.string "other_vehicle_owner_address_town"
+    t.string "other_vehicle_owner_address_zip"
+    t.string "other_vehicle_owner_cell_phone"
+    t.string "other_vehicle_owner_home_phone"
+    t.string "other_vehicle_owner_name"
+    t.string "other_vehicle_owner_work_phone"
+    t.integer "other_vehicle_passengers"
+    t.string "other_vehicle_plate"
+    t.string "other_vehicle_state"
+    t.boolean "other_vehicle_towed_from_scene"
+    t.string "other_vehicle_year"
+    t.boolean "passenger_incident", default: false
+    t.integer "passengers_onboard"
+    t.string "point_of_impact"
+    t.string "police_badge_number"
+    t.string "police_case_assigned"
+    t.boolean "police_on_scene", default: false
+    t.string "police_town_or_state"
+    t.integer "posted_speed_limit"
+    t.boolean "property_owner_information_taken", default: false
     t.boolean "pvta_passenger_information_taken", default: false
+    t.string "reason_not_up_to_curb"
+    t.string "road_conditions"
+    t.string "route"
+    t.string "run"
+    t.integer "speed"
+    t.string "state"
+    t.text "summons_or_warning_info"
+    t.boolean "summons_or_warning_issued", default: false
+    t.string "surface_grade"
+    t.string "surface_type"
+    t.boolean "towed_from_scene"
+    t.string "town"
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "user_id"
+    t.text "vehicle_distance"
+    t.string "vehicle_in_bus_stop_plate"
+    t.string "weather_conditions"
+    t.boolean "wheelchair_involved", default: false
+    t.string "zip"
   end
 
   create_table "incidents", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.integer "driver_incident_report_id"
-    t.integer "supervisor_incident_report_id"
-    t.integer "supervisor_report_id"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.boolean "completed", default: false
-    t.integer "reason_code_id"
     t.string "claim_number"
+    t.integer "claims_id"
+    t.boolean "completed", default: false
+    t.datetime "created_at", precision: nil, null: false
+    t.integer "driver_incident_report_id"
+    t.boolean "exported_to_claims"
     t.boolean "exported_to_hastus", default: false
-    t.string "second_reason_code"
-    t.text "root_cause_analysis"
-    t.boolean "preventable"
     t.string "latitude"
     t.string "longitude"
-    t.boolean "video_pulled"
-    t.integer "claims_id"
-    t.boolean "exported_to_claims"
+    t.boolean "preventable"
+    t.integer "reason_code_id"
+    t.text "root_cause_analysis"
+    t.string "second_reason_code"
+    t.integer "supervisor_incident_report_id"
+    t.integer "supervisor_report_id"
     t.integer "supplementary_reason_code_id"
+    t.datetime "updated_at", precision: nil, null: false
+    t.boolean "video_pulled"
   end
 
   create_table "injured_passengers", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "name"
     t.text "address"
+    t.string "cell_phone"
+    t.string "home_phone"
+    t.integer "incident_report_id"
+    t.string "name"
     t.string "nature_of_injury"
     t.boolean "transported_to_hospital"
-    t.string "home_phone"
-    t.string "cell_phone"
     t.string "work_phone"
-    t.integer "incident_report_id"
   end
 
   create_table "reason_codes", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "identifier"
     t.text "description"
+    t.string "identifier"
   end
 
   create_table "staff_reviews", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.integer "incident_id"
-    t.integer "user_id"
-    t.text "text"
     t.datetime "created_at", precision: nil, null: false
+    t.integer "incident_id"
+    t.text "text"
     t.datetime "updated_at", precision: nil, null: false
+    t.integer "user_id"
   end
 
   create_table "supervisor_reports", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.boolean "pictures_saved"
-    t.integer "saved_pictures"
-    t.text "passenger_statement"
-    t.datetime "faxed", precision: nil
+    t.text "amplifying_comments"
+    t.boolean "completed_alcohol_test"
     t.boolean "completed_drug_or_alcohol_test"
+    t.boolean "completed_drug_test"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "director_notified_at", precision: nil
+    t.string "employee_appearance"
+    t.datetime "employee_arrived_at_test_at", precision: nil
+    t.string "employee_behavior"
+    t.datetime "employee_departed_to_test_at", precision: nil
+    t.datetime "employee_notified_of_test_at", precision: nil
+    t.string "employee_odor"
+    t.datetime "employee_representative_arrived_at", precision: nil
+    t.datetime "employee_representative_notified_at", precision: nil
+    t.datetime "employee_returned_to_work_or_released_from_duty_at", precision: nil
+    t.string "employee_speech"
+    t.datetime "faxed", precision: nil
+    t.datetime "observation_made_at", precision: nil
+    t.text "passenger_statement"
+    t.boolean "pictures_saved"
+    t.datetime "program_manager_notified_at", precision: nil
+    t.text "reason_driver_discounted"
+    t.text "reason_threshold_not_met"
+    t.integer "saved_pictures"
+    t.datetime "superintendent_notified_at", precision: nil
+    t.boolean "test_due_to_bodily_injury"
+    t.boolean "test_due_to_disabling_damage"
+    t.boolean "test_due_to_employee_appearance"
+    t.boolean "test_due_to_employee_behavior"
+    t.boolean "test_due_to_employee_odor"
+    t.boolean "test_due_to_employee_speech"
+    t.boolean "test_due_to_fatality"
+    t.datetime "test_ended_at", precision: nil
+    t.datetime "test_started_at", precision: nil
     t.string "test_status"
     t.string "testing_facility"
     t.datetime "testing_facility_notified_at", precision: nil
-    t.datetime "employee_notified_of_test_at", precision: nil
-    t.datetime "employee_departed_to_test_at", precision: nil
-    t.datetime "employee_arrived_at_test_at", precision: nil
-    t.datetime "test_started_at", precision: nil
-    t.datetime "test_ended_at", precision: nil
-    t.datetime "employee_returned_to_work_or_released_from_duty_at", precision: nil
-    t.datetime "superintendent_notified_at", precision: nil
-    t.datetime "program_manager_notified_at", precision: nil
-    t.datetime "director_notified_at", precision: nil
-    t.text "amplifying_comments"
-    t.boolean "test_due_to_bodily_injury"
-    t.boolean "test_due_to_disabling_damage"
-    t.boolean "test_due_to_fatality"
-    t.boolean "completed_drug_test"
-    t.boolean "completed_alcohol_test"
-    t.datetime "observation_made_at", precision: nil
-    t.boolean "test_due_to_employee_appearance"
-    t.string "employee_appearance"
-    t.boolean "test_due_to_employee_behavior"
-    t.string "employee_behavior"
-    t.boolean "test_due_to_employee_speech"
-    t.string "employee_speech"
-    t.boolean "test_due_to_employee_odor"
-    t.string "employee_odor"
-    t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
-    t.text "reason_threshold_not_met"
-    t.text "reason_driver_discounted"
-    t.datetime "employee_representative_notified_at", precision: nil
-    t.datetime "employee_representative_arrived_at", precision: nil
   end
 
   create_table "supplementary_reason_codes", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "identifier"
     t.string "description"
+    t.string "identifier"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.boolean "staff", default: false
-    t.string "badge_number"
     t.boolean "active", default: true
-    t.boolean "supervisor", default: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "badge_number"
+    t.datetime "created_at", precision: nil, null: false
     t.string "division"
     t.string "email"
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
+    t.string "first_name"
+    t.string "last_name"
     t.boolean "password_changed_from_default", default: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
+    t.boolean "staff", default: false
+    t.boolean "supervisor", default: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "versions", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
-    t.string "item_type", limit: 191, null: false
-    t.integer "item_id", null: false
-    t.string "event", null: false
-    t.string "whodunnit"
     t.datetime "created_at", precision: nil
+    t.string "event", null: false
+    t.integer "item_id", null: false
+    t.string "item_type", limit: 191, null: false
     t.text "object", size: :long, collation: "utf8mb4_bin"
     t.text "object_changes", size: :long, collation: "utf8mb4_bin"
+    t.string "whodunnit"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
     t.check_constraint "json_valid(`object_changes`)", name: "object_changes"
     t.check_constraint "json_valid(`object`)", name: "object"
   end
 
   create_table "witnesses", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.integer "supervisor_report_id"
-    t.string "name"
     t.text "address"
-    t.boolean "onboard_bus"
-    t.string "home_phone"
     t.string "cell_phone"
-    t.string "work_phone"
     t.datetime "created_at", precision: nil, null: false
+    t.string "home_phone"
+    t.string "name"
+    t.boolean "onboard_bus"
+    t.integer "supervisor_report_id"
     t.datetime "updated_at", precision: nil, null: false
+    t.string "work_phone"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
